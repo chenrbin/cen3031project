013277b65a86102196981bd12d5b915131adef3a
Leo Jiang <114763373+leojiangxd@users.noreply.github.com>
2024-04-14 01:10:47 -0400

    login/register fail message

--- src/pages/User/Login.js
+++ src/pages/User/Login.js
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
-import { Button, TextField, Stack, Typography } from '@mui/material';
-import { handleLogin, handleRegister } from '../../API';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, TextField, Stack, Typography } from "@mui/material";
+import { handleLogin, handleRegister } from "../../API";
+import { useNavigate } from "react-router-dom";
 
 const UserLogin = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const buttonStyle = {
-    marginTop: '45px',
-    textDecoration: 'none',
-    width: '200px',
-    textAlign: 'center',
-    background: '#FF2625',
-    padding: '12px',
-    fontSize: '22px',
-    textTransform: 'none',
-    color: 'white',
-    borderRadius: '4px',
-    '&:hover': {
-      color: '#FF2625',
-      background: '#fff', // Change hover color to a darker shade of red
+    marginTop: "45px",
+    textDecoration: "none",
+    width: "200px",
+    textAlign: "center",
+    background: "#FF2625",
+    padding: "12px",
+    fontSize: "22px",
+    textTransform: "none",
+    color: "white",
+    borderRadius: "4px",
+    "&:hover": {
+      color: "#FF2625",
+      background: "#fff", // Change hover color to a darker shade of red
     },
   };
 
@@ -31,9 +32,13 @@ const UserLogin = ({ onLogin }) => {
       if (response === 200) {
         onLogin(true);
         navigate("/");
+      } else {
+        setErrorMessage("Incorrect username or password.");
+        console.log("ERROR" + errorMessage); // Check if errorMessage is updated
       }
     } catch (error) {
       console.error("Login failed:", error);
+      setErrorMessage("Incorrect username or password.");
     }
   };
 
@@ -43,26 +48,40 @@ const UserLogin = ({ onLogin }) => {
       if (response === 200) {
         onLogin(true);
         navigate("/");
+      } else {
+        setErrorMessage(
+          "Username must be alphanumeric and password must be at least 6 characters."
+        );
+        console.log("ERROR" + errorMessage); // Check if errorMessage is updated
       }
     } catch (error) {
       console.error("Registration failed:", error);
+      setErrorMessage(
+        "Username must be alphanumeric and password must be at least 6 characters."
+      );
     }
   };
-  
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' }, textAlign: 'center', mb: '49px' }}>
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "30px" },
+          textAlign: "center",
+          mb: "49px",
+        }}
+      >
         User Login
       </Typography>
-      <form style={{ width: '100%', maxWidth: '400px' }}>
+      <form style={{ width: "100%", maxWidth: "400px" }}>
         <Stack spacing={2}>
           <TextField
             label="Username"
             variant="outlined"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            sx={{ width: '100%' }}
+            sx={{ width: "100%" }}
           />
           <TextField
             label="Password"
@@ -70,16 +89,33 @@ const UserLogin = ({ onLogin }) => {
             variant="outlined"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            sx={{ width: '100%' }}
+            sx={{ width: "100%" }}
           />
           <Stack direction="row" spacing={5} justifyContent="center">
-            <Button variant="contained" onClick={handleLoginClick} sx={{ ...buttonStyle, width: '100%' }}>
+            <Button
+              variant="contained"
+              onClick={handleLoginClick}
+              sx={{ ...buttonStyle, width: "100%" }}
+            >
               Login
             </Button>
-            <Button variant="contained" onClick={handleRegisterClick} sx={{ ...buttonStyle, width: '100%' }}>
+            <Button
+              variant="contained"
+              onClick={handleRegisterClick}
+              sx={{ ...buttonStyle, width: "100%" }}
+            >
               Register
             </Button>
           </Stack>
+          {errorMessage && (
+            <Typography
+              variant="body2"
+              color="error"
+              sx={{ textAlign: "center" }}
+            >
+              {errorMessage}
+            </Typography>
+          )}
         </Stack>
       </form>
     </Stack>
