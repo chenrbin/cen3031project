272c87dcd8176b152fef99cd7f48b8e34767c7fc
Hoang Tran <tranvietnguyenhoang2004@gmail.com>
2024-04-17 21:04:32 -0400

    Finish recommended club feature

--- src/pages/ClubDetail.js
+++ src/pages/ClubDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 import Detail from '../components/Detail';
-//import SimilarClub from '../components/SimilarClub';
+import SimilarClub from '../components/SimilarClub';
 import axios from "axios";
 const ClubDetail = () => {
   const [clubDetail, setClubDetail] = useState({});
@@ -14,7 +14,6 @@ const ClubDetail = () => {
         .get("http://localhost:5000/Club/" + id)
         .then((response) => {
           setClubDetail(response.data);
-          console.log(response.data);
         })
         .catch((err) => console.log(err));
     }
@@ -24,6 +23,7 @@ const ClubDetail = () => {
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail clubDetail={clubDetail} />
+      <SimilarClub clubDetail = {clubDetail}/>
     </Box>
   );  
 };
